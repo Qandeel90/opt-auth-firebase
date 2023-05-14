6cc3b0cf90cbf5b90b429918de3b6cbab2c342b3
qandeel786 <qandeel@karigar.pk>
2023-05-15 02:05:01 +0500

    v

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlcXZQJO-sMscgL5kkuAIzexIS7MPWE5U",
-  authDomain: "fir-auth-bd87b.firebaseapp.com",
-  projectId: "fir-auth-bd87b",
-  storageBucket: "fir-auth-bd87b.appspot.com",
-  messagingSenderId: "758942184853",
-  appId: "1:758942184853:web:af668418ebc0f0cd94b29c",
+  apiKey: "AIzaSyBwp1vhUiPuDXkUxAry9bArekEN6noLugY",
+  authDomain: "otp-auth-firebase-1.firebaseapp.com",
+  projectId: "otp-auth-firebase-1",
+  storageBucket: "otp-auth-firebase-1.appspot.com",
+  messagingSenderId: "264039204633",
+  appId: "1:264039204633:web:b0c8b1b02a213c1640e788",
 };
 
 // Initialize Firebase
